79eae8fab7db6f76c2e713b3090b29a0c6764dcf
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-04-18 23:52:01 +0300

    change specs directory for pythonmop

--- scripts/run-mop-new.sh
+++ scripts/run-mop-new.sh
@@ -17,7 +17,7 @@ call_pymop(){
 
     #run and print this command
     set -x
-    timeout 10800 pytest -p pythonmop -p monitor -rA  --path=/projects/mop-with-dynapt/specs-new/ --algo $algo \
+    timeout 10800 pytest -p pythonmop -p monitor -rA  --path="$PWD"/../mop-with-dynapt/specs-new/ --algo $algo \
     --continue-on-collection-errors --json-report --json-report-indent=2  --description ALGO_$algo --statistics --statistics_file="$algo".json $extra_args &> $report/pymop_$algo.out
     # if process stop by timeout, then print timeout
     if [ $? -eq 124 ]; then
