fc2bce96aae092dd55f309dd1e0b4cce87dbf772
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2025-05-22 10:22:33 +0300

    remove env variable

--- scripts/run-mop-new.sh
+++ scripts/run-mop-new.sh
@@ -36,7 +36,10 @@ call_pymop(){
         ls -l "$PWD"/../mop-with-dynapt/pythonmop/pymop-startup-helper/
         echo "================================================"
 
-        PYTHONPATH="$PWD"/../mop-with-dynapt/pythonmop/pymop-startup-helper/ timeout 14400 pytest --color=no -v -p pythonmop -rA --path="$PWD"/../mop-with-dynapt/specs-new/ --algo $algo --memray --trace-python-allocators --most-allocations=0 --memray-bin-path=$report/MEM_$algo \
+        # PYTHONPATH="$PWD"/../mop-with-dynapt/pythonmop/pymop-startup-helper/ timeout 14400 pytest --color=no -v -p pythonmop -rA --path="$PWD"/../mop-with-dynapt/specs-new/ --algo $algo --memray --trace-python-allocators --most-allocations=0 --memray-bin-path=$report/MEM_$algo \
+        # --continue-on-collection-errors --json-report --json-report-indent=2 --statistics --statistics_file="$algo".json $extra_args &> $report/$algo-pytest-output.txt
+
+        timeout 14400 pytest --color=no -v -p pythonmop -rA --path="$PWD"/../mop-with-dynapt/specs-new/ --algo $algo --memray --trace-python-allocators --most-allocations=0 --memray-bin-path=$report/MEM_$algo \
         --continue-on-collection-errors --json-report --json-report-indent=2 --statistics --statistics_file="$algo".json $extra_args &> $report/$algo-pytest-output.txt
     fi
     
