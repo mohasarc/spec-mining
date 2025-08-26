201a8166ebcf710e81646fc879f02d13ce207200
Stephen Shen <Stephen_Shen0512@163.com>
2025-08-26 11:00:14 -0400

    Update spec path

--- DyLin_DynaPyt_Experiment/run_dynapyt_libs.sh
+++ DyLin_DynaPyt_Experiment/run_dynapyt_libs.sh
@@ -122,7 +122,7 @@ export DYNAPYT_SESSION_ID=$(uuidgen)
 echo "DynaPyt Session ID: $DYNAPYT_SESSION_ID"
 
 # Copy the analyses file to temp directory with session ID
-cp "$PWD/../Specs/dynapyt_analyses.txt" "$TMPDIR/dynapyt_analyses-$DYNAPYT_SESSION_ID.txt"
+cp "$PWD/../Specs_libs/dynapyt_analyses.txt" "$TMPDIR/dynapyt_analyses-$DYNAPYT_SESSION_ID.txt"
 
 # Display contents of the copied file
 cat "$TMPDIR/dynapyt_analyses-$DYNAPYT_SESSION_ID.txt"
