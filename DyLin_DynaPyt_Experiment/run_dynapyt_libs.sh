03192a5ad1878796ff3ae2ce7b7f20b5c30f6ad1
Zhuohang Shen <Stephen_Shen0512@163.com>
2025-08-26 00:05:09 -0400

    Update file paths in run_dynapyt_libs.sh

--- DyLin_DynaPyt_Experiment/run_dynapyt_libs.sh
+++ DyLin_DynaPyt_Experiment/run_dynapyt_libs.sh
@@ -122,7 +122,7 @@ export DYNAPYT_SESSION_ID=$(uuidgen)
 echo "DynaPyt Session ID: $DYNAPYT_SESSION_ID"
 
 # Copy the analyses file to temp directory with session ID
-cp "$PWD/../Specs_libs/dynapyt_analyses.txt" "$TMPDIR/dynapyt_analyses-$DYNAPYT_SESSION_ID.txt"
+cp "$PWD/../Specs/dynapyt_analyses.txt" "$TMPDIR/dynapyt_analyses-$DYNAPYT_SESSION_ID.txt"
 
 # Display contents of the copied file
 cat "$TMPDIR/dynapyt_analyses-$DYNAPYT_SESSION_ID.txt"
@@ -163,7 +163,7 @@ cd ..
 git clone "$DYNAPYT_REPO_URL" "$(basename $DYNAPYT_REPO_URL .git)_virtual" || { echo "Failed to clone $DYNAPYT_REPO_URL"; exit 1; }
 
 # Specify the source directory containing the Python DynaPyt files
-SOURCE_DIR="$PWD/../Specs_libs/DynaPyt"
+SOURCE_DIR="$PWD/../Specs/DynaPyt"
 
 # Define the destination directory in the cloned DynaPyt repository
 DESTINATION_DIR="$PWD/DynaPyt_virtual/src/dynapyt/analyses"
